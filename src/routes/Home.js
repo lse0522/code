3200a606c43594d362465e1bea4190d455ddd982
sangpyokim <rlatkdvy12@naver.com>
2022-10-12 00:46:36 +0900

    asdasd
    asd

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -5,21 +5,8 @@ function Home() {
   return (
     <div>
       <Button variant="primary">Primary</Button>
-      <Button variant="outline-dark">Dark</Button>
-      {[
-        'primary',
-        'secondary',
-        'success',
-        'danger',
-        'warning',
-        'info',
-        'light',
-        'dark',
-      ].map((variant) => (
-        <Alert key={variant} variant={variant}>
-          This is a {variant} alert—check it out!
-        </Alert>
-      ))}
+      <Button type='button' className='btn btn-danger' >Dark</Button>
+
     </div>
   )
 }
